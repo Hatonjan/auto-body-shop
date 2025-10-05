2c2ae976fa58c95ebf8c784e0718f3adca2ca743
Jonathan <mj.scot.r@gmail.com>
2025-10-04 18:31:54 -0700

    Remove the text in the hero section from the video and set it at the top of the video

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 // Initialize the AOS animations
 AOS.init({
-    offset: 120, // triggers 120px before element enters view
-    duration: 1000,
-    once: false, // allow animation every time it scrolls into view
-    mirror: false // disable animation on scroll up
+    offset: 120,     // triggers 120px before element enters view
+    duration: 1000, // The duration in millisecond for the animation
+    once: false,    // allow animation every time it scrolls into view
+    mirror: false  // disable animation on scroll up
 });
 
 
